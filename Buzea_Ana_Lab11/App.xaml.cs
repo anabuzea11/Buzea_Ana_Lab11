48deccfc19e38809eb6f595e1685f2cdba709393
Buzea Ana <anabuzea99@gmail.com>
2024-12-29 18:30:48 +0200

    Add project files.

---
+++ Buzea_Ana_Lab11/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using Buzea_Ana_Lab11.Data;
+
+namespace Buzea_Ana_Lab11 
+{
+    public partial class App : Application
+    {
+        public static ShoppingListDatabase Database { get; private set; }
+        public App()
+        {
+            InitializeComponent();
+
+            Database = new ShoppingListDatabase(new RestService()); 
+            MainPage = new NavigationPage(new ListEntryPage());
+        }
+    }
+}
---
+++ Buzea_Ana_Lab11/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Buzea_Ana_Lab11
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
